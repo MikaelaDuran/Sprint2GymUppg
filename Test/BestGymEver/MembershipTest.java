9c7d55f13b24bfec0d45e1e1ba5dc08b3f438806
Mikaela Duran <mikaeladuran93@gmail.com>
2024-10-21 01:49:01 +0200

    first commit

--- Test/BestGymEver/MembershipTest.java
+++ Test/BestGymEver/MembershipTest.java
@@ -1,10 +1,17 @@
 package BestGymEver;
 
+import java.time.LocalDate;
+
 public class MembershipTest {
 
     // Att inläsning av korrekt data görs,på korrekt sätt
     // Att datat kontrolleras och behandlas på rätt sätt
     // Att korrekta utskrifter skrivs till fil
+
+    private LocalDate currentDate;
+    private Membership membership;
+
+
 
 
 }
